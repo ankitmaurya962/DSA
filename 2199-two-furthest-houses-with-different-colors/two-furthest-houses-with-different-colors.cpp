3e02d59579cde2f19ba306ca75cbc7f38fd77c55
ankitmaurya962 <ankitmaurya36626@gmail.com>
2026-04-20 19:32:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (40.24%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
@@ -4,16 +4,15 @@ public:
         int n = colors.size();
 
         int maxDis = 0;
-        for(int i = 0; i<n; i++){
-            for(int j = n-1; j>i; j--){
 
-                if(colors[i]!=colors[j]){
-                    maxDis = max(maxDis, j-i);
-                }
-                
-            }
+        for(int i = n-1; i>0; i--){
+            if(colors[i]!=colors[0]){
+                maxDis = max(maxDis, i-0);
+            } 
+            if(colors[n-i]!=colors[n-1]){
+                maxDis = max(maxDis, n-1-(n-i));
+            } 
         }
-
         return maxDis;
     }
 };
