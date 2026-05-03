d2fd7d50cb441e333936a5d11036a8020877a939
ankitmaurya962 <ankitmaurya36626@gmail.com>
2026-05-03 12:39:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (84.87%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n=s.length();
+        int m=goal.length();
+        if(n!=m) return false;
+        if((s+s).find(goal)!=string::npos){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
