f059a73380dc95c6dcf0d49148296a218fd24f88
ankitmaurya962 <ankitmaurya36626@gmail.com>
2026-05-26 09:44:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (27.15%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        unordered_map<char, int>smallMp;
-        unordered_map<char, int>capMp;
+        unordered_map<char, int>mp;
 
         for(int i = 0; i<word.size(); i++){
-            if(word[i]>='a' && word[i]<='z'){
-                smallMp[word[i]]++;
-            }else{
-                capMp[word[i]]++;
-            }
+            mp[word[i]]++;
         }
 
         int count = 0;
 
         for(int i = 0; i<word.size(); i++){
             if(word[i]>='a' && word[i]<='z'){
-                if(capMp.find(word[i]-32)!=capMp.end()){
+                if(mp.find(word[i]-32)!=mp.end()){
                     count++;
-                    smallMp.erase(word[i]);
-                    capMp.erase(word[i]-32);
+                    mp.erase(word[i]);
+                    mp.erase(word[i]-32);
                 }
             }
         }
