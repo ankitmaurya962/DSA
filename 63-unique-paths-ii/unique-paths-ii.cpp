5af0e4ad5ce54a96fe865c39806c085f52c61d82
ankitmaurya962 <ankitmaurya36626@gmail.com>
2026-09-16 14:50:22 +0530

    Time: 1 ms (15.33%) | Memory: 11.9 MB (30.77%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>>& obstacleGrid, vector<vector<int>>&dp){
-        if(i < 0 || j < 0 || obstacleGrid[i][j] == 1) return 0;
-        if(i == 0 && j == 0) return 1;
+    int f(int i, int j, vector<vector<int>>& obstacleGrid,
+          vector<vector<int>>& dp) {
+        if (i < 0 || j < 0 || obstacleGrid[i][j] == 1)
+            return 0;
+        if (i == 0 && j == 0)
+            return 1;
 
-        if(dp[i][j]!=-1) return dp[i][j];
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
-        int right = f(i, j-1, obstacleGrid, dp);
-        int down = f(i-1, j, obstacleGrid, dp);
+        int right = f(i, j - 1, obstacleGrid, dp);
+        int down = f(i - 1, j, obstacleGrid, dp);
 
         return dp[i][j] = right + down;
     }
@@ -15,7 +19,28 @@ public:
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
 
-        vector<vector<int>>dp(n, vector<int>(m, -1));
-        return f(n-1, m-1, obstacleGrid, dp);
+        vector<vector<int>> dp(n, vector<int>(m));
+        dp[0][0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (i == 0 && j == 0)
+                    continue;
+
+                int right = 0;
+                int down = 0;
+
+                if (obstacleGrid[i][j] == 0) {
+                    if (j > 0)
+                        right = dp[i][j - 1];
+                    if (i > 0)
+                        down = dp[i - 1][j];
+                }
+
+                dp[i][j] = right + down;
+            }
+        }
+        return dp[n - 1][m - 1];
     }
 };
