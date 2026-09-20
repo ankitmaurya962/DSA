e8cebd3c93a51aaf582fc7b0b025b3be59dac3cb
ankitmaurya962 <ankitmaurya36626@gmail.com>
2026-09-20 11:34:57 +0530

    Time: 35 ms (42.53%) | Memory: 36.2 MB (67.41%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
-    int f(vector<int>&coins, int amount, int n, vector<vector<int>>&dp){
-        if(amount == 0) return 1;
-        if(n == 0){
-            if(amount % coins[n] == 0) return 1;
-            return 0;
-        }
+    // int f(vector<int>&coins, int amount, int n, vector<vector<int>>&dp){
+    //     if(amount == 0) return 1;
+    //     if(n == 0){
+    //         if(amount % coins[n] == 0) return 1;
+    //         return 0;
+    //     }
 
-        if(dp[n][amount]!=-1) return dp[n][amount];
-        int pick = 0;
-        if(amount >= coins[n]) pick = f(coins, amount-coins[n], n, dp);
-        int notPick = f(coins, amount, n-1, dp);
+    //     if(dp[n][amount]!=-1) return dp[n][amount];
+    //     int pick = 0;
+    //     if(amount >= coins[n]) pick = f(coins, amount-coins[n], n, dp);
+    //     int notPick = f(coins, amount, n-1, dp);
 
-        return dp[n][amount] = pick + notPick;
-    }
+    //     return dp[n][amount] = pick + notPick;
+    // }
+
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>>dp(n, vector<int>(amount + 1, -1));
+        vector<vector<int>>dp(n, vector<int>(amount + 1, 0));
         
-        return f(coins, amount, n-1 ,dp);
+        for(int i = 0; i<n; i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i = 0; i<=amount; i++){
+            if(i % coins[0] == 0) dp[0][i] = 1;
+            else dp[0][i] = 0;
+        }
+
+        for(int i = 1; i<n; i++){
+            for(int j = 1; j<=amount; j++){
+                long long pick = 0;
+                if(j >= coins[i]) pick = (dp[i][j-coins[i]]); 
+                long long notPick = dp[i-1][j];
+
+                dp[i][j] = pick + notPick;
+            }
+        }
+
+        return (int)dp[n-1][amount];
     }
 };
