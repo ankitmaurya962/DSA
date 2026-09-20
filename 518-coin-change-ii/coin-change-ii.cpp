b681a191b95af5cace318d58091b6eb0eb2a9ee6
ankitmaurya962 <ankitmaurya36626@gmail.com>
2026-09-20 11:04:02 +0530

    Time: 21 ms (75.47%) | Memory: 36.5 MB (56.92%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int f(int amount, vector<int>&coins, int n, vector<vector<int>>&dp){
+    int f(vector<int>&coins, int amount, int n, vector<vector<int>>&dp){
         if(amount == 0) return 1;
         if(n == 0){
-            if(amount % coins[n] == 0){
-                return 1;
-            } 
-            else return 0;
+            if(amount % coins[n] == 0) return 1;
+            return 0;
         }
-        if(dp[n][amount] != -1) return dp[n][amount];
-        int notPick = f(amount, coins, n-1, dp);
-        int pick = 0;
-        if(coins[n] <= amount) pick = f(amount - coins[n], coins, n, dp);
 
-        return dp[n][amount] = notPick + pick;
+        if(dp[n][amount]!=-1) return dp[n][amount];
+        int pick = 0;
+        if(amount >= coins[n]) pick = f(coins, amount-coins[n], n, dp);
+        int notPick = f(coins, amount, n-1, dp);
+
+        return dp[n][amount] = pick + notPick;
     }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         vector<vector<int>>dp(n, vector<int>(amount + 1, -1));
-        return f(amount, coins, n-1, dp);
+        
+        return f(coins, amount, n-1 ,dp);
     }
 };
