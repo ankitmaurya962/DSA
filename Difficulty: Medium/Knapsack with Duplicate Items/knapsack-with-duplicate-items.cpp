87880f8efb9641f9d031a3b699bd3e1cb8e54edf
ankitmaurya962 <ankitmaurya36626@gmail.com>
2026-09-21 00:26:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
@@ -1,27 +1,26 @@
 class Solution {
-public:
-    int f(int n, int w, vector<int>& wt, vector<int>& val,
-          vector<vector<int>>& dp) {
-
-        if (n == 0) {
-            return (w / wt[0]) * val[0];
+  public:
+  
+    int f(vector<int>&val, vector<int>&wt, int capacity, int n, vector<vector<int>>&dp){
+        if(n == 0) {
+            if(wt[0] <= capacity) return (capacity/wt[n]) * val[0];
+            
+            return 0;
         }
-
-        if (dp[n][w] != -1)
-            return dp[n][w];
-
-        int notPick = f(n - 1, w, wt, val, dp);
-
+        
+        if(dp[n][capacity] != -1) return dp[n][capacity];
         int pick = 0;
-        if (wt[n] <= w)
-            pick = val[n] + f(n, w - wt[n], wt, val, dp);
-
-        return dp[n][w] = max(pick, notPick);
+        if(capacity >= wt[n]) pick = val[n] + f(val, wt, capacity-wt[n], n, dp);
+        int notPick = f(val, wt, capacity, n-1, dp);
+        
+        return dp[n][capacity] = max(pick, notPick);
     }
-
+    
     int knapSack(vector<int>& val, vector<int>& wt, int capacity) {
+        // code here
+        
         int n = val.size();
-        vector<vector<int>> dp(n, vector<int>(capacity + 1, -1));
-        return f(n - 1, capacity, wt, val, dp);
+        vector<vector<int>>dp(n, vector<int>(capacity + 1, -1));
+        return f(val, wt, capacity, n-1, dp);
     }
 };
