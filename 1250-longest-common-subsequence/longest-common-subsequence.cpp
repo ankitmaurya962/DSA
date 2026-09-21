9ceac25c162967b5921626cd9081dcf031751f93
ankitmaurya962 <ankitmaurya36626@gmail.com>
2026-09-21 23:44:29 +0530

    Time: 45 ms (17.97%) | Memory: 27.6 MB (19.19%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    int f(int i, int j, string &text1, string &text2, vector<vector<int>>&dp){
-        if(i<0 || j<0) return 0;
+    int f(string &text1, string &text2, int i, int j, vector<vector<int>>&dp){
+
+        if(i < 0 || j < 0) return 0;
 
         if(dp[i][j] != -1) return dp[i][j];
-        if(text1[i] == text2[j]){
-            return 1 + f(i - 1, j - 1, text1, text2, dp);
+
+        //match
+        if(text1[i] == text2[j]) {
+            return dp[i][j] = 1 + f(text1, text2, i - 1, j - 1, dp);
         }
 
-        return dp[i][j] = max(f(i - 1, j, text1, text2, dp), f(i, j - 1, text1, text2, dp));
+        //notmatch
+        return dp[i][j] = max(f(text1, text2, i - 1, j, dp), f(text1, text2, i, j - 1, dp));
     }
     int longestCommonSubsequence(string text1, string text2) {
         int n1 = text1.size();
         int n2 = text2.size();
 
         vector<vector<int>>dp(n1, vector<int>(n2, -1));
-        return f(n1-1, n2-1, text1, text2, dp);
+        return f(text1, text2, n1-1, n2-1, dp);
     }
 };
