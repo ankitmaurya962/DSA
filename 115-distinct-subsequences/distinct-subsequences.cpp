6bb6ba479352d37c439ffd6b30076a3825388780
ankitmaurya962 <ankitmaurya36626@gmail.com>
2026-09-26 00:15:32 +0530

    Time: 29 ms (53.51%) | Memory: 44.3 MB (18.38%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    int f(string &s, string &t, int n1, int n2, vector<vector<int>>&dp){
-        if(n2 == 0) return 1;
-        if(n1 == 0) return 0;
-
-        if(dp[n1][n2] != -1) return dp[n1][n2];
-
-        if(s[n1-1] == t[n2-1]) return dp[n1][n2] = f(s, t, n1-1, n2-1, dp) + f(s, t, n1-1, n2, dp);
-
-        return dp[n1][n2] = f(s, t, n1-1, n2, dp);
-    }
     int numDistinct(string s, string t) {
         int n1 = s.size();
         int n2 = t.size();
 
-        vector<vector<int>>dp(n1+1, vector<int>(n2+1, -1));
-        return f(s, t, n1, n2, dp);
+        vector<vector<long long>>dp(n1+1, vector<long long>(n2+1, 0));
+
+        for(int i = 0; i<=n1; i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i = 1; i<=n2; i++){
+            dp[0][i] = 0;
+        }
+        for(int i = 1; i<=n1; i++){
+            for(int j = 1; j<=n2; j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = min((long long)INT_MAX,dp[i-1][j-1] + dp[i-1][j]);
+                }
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n1][n2];
     }
 };
